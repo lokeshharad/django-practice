af5a67205bbf6b7975969bf6a5923fd6b9f73553
lokeshharad <lokeshharad@gmail.com>
2021-01-10 17:51:20 +0530

    working with templates Part A:
    	1. created a templates dir in blog and another blog ( same name as app) dir in templates dir
    	2. created home.html, about.html files
       		home.html : default html code for home page
       		about.html : default html code about page
    
    	3. blog/apps.py : class BlogConfig app added to INSTALLED APP in django_project/settings.py
    	4. blog/views.py : render the above html pages to this python script
    
    Part B:
    
    	1. blog/views.py: added dummy posts as list of dictionaries
    			  added context as dictionary to home function/route.
    	2. blog/templates/blog/home.html : display those dummy posts in home page using for loop
    					   title for page if available otherwise default title using if else
    	   blog/templates/blog/about.html : title for page if available other default title using if else
    
    Part C:
    
    	1. blog/templates/blog/base.html : base.html is created for TEMPLATE INHERITANCE.
    					   above if else part is added to this html file which is to be extended further
    					   created a block in body tag
    					   and this base.html is extended in home and about page
    
    Part D: bootstrap templates
    
    	1. the starter bootstrap template is added to base.html in head tag abd body tag
    	2. code snippets are added, navigation
    	3. blog/static/blog/main.css : static dir created and main.css is added. static dir loaded. article tag added.
    	4. urls from blog/urls.py are added to base.html file to access the home, about, etc django urls.

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -6,6 +6,6 @@ from . import views
 # ]
 
 urlpatterns = [
-    path("", views.home, name="blog-name"),
+    path("", views.home, name="blog-home"),
     path("about/", views.about, name="blog-about"),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,8 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'CoreyMS', 
+        'title': 'Blog Post 1', 
+        'content': 'First post content', 
+        'date_posted': 'January 10, 2021'
+    }, 
+    {
+        'author': 'Lokesh Harad', 
+        'title': 'Blog Post 2',
+        'content': 'Second post content', 
+        'date_posted': 'February 10, 2021'
+    }
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    
+    context = {
+        'posts': posts
+    }
+    # return HttpResponse("<h1>Blog Home</h1>")
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    # return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
